6783b5094f6fb051fd9d977db81ccf4c9edb5fde
Vishal-Malo <vishalmalo272000@gmail.com>
2019-10-30 23:59:32 +0530

    KNN Algorithm from scratch

--- KNNTest.py
+++ KNNTest.py
@@ -9,11 +9,10 @@ x = data.iloc[:, 0:4].values
 y = data.iloc[:, -1].values
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.25)
-knn = KNearestNeighbours(k=11)
-knn.fit(x_train=x_train,y_train=y_train)
+knn = KNearestNeighbours(k=11, weights="uniform")
+knn.fit(x_train=x_train, y_train=y_train)
 
 y_pred = knn.predict(np.array(x_test).reshape(len(x_test), 4))
-print("y_pred =" , y_pred)
 score = 0
 for i in range(len(y_test)):
     if y_test[i] == y_pred[i]:
--- KNearestNeighbour.py
+++ KNearestNeighbour.py
@@ -1,44 +1,69 @@
-import  operator
+import numpy as np
+import operator
 from collections import Counter
 
+
 class KNearestNeighbours:
-    def __init__(self,k):
+    def __init__(self, k, weights):
         self.k = k
+        self.weights = weights
         self.result = []
+        self.result_w = []
 
     def fit(self, x_train, y_train):
         self.x_train = x_train
         self.y_train = y_train
-        print("Tranning done!")
+        print("Training done!")
 
     def predict(self, x_test):
-
         for j in x_test:
             distance = {}
+            distance_w = {}
             counter = 0
 
             for i in self.x_train:
                 sum = 0
                 for k in range(len(j)):
-                    sum += (j[k] - i[k])**2
+                    sum += (j[k] - i[k]) ** 2
 
-                distance[counter] = (sum)**1/2
+                if self.weights == "uniform":
+                    distance[counter] = sum ** 1 / 2
+                elif self.weights == "distance":
+                    distance_w[counter] = 1 / (sum ** 1 / 2)
                 counter += 1
 
-            distance = sorted(distance.items(), key=operator.itemgetter(1))
-            self.result.append(self.classify(distance[:self.k]))
-            del(distance)
+            if self.weights == "uniform":
+                distance = sorted(distance.items(), key=operator.itemgetter(1))
+                self.result.append(self.classify(distance[:self.k]))
+                del distance
 
-        return  self.result
+            elif self.weights == "distance":
+                distance_w = sorted(distance_w.items(), key=operator.itemgetter(1), reverse=True)
+                self.result_w.append(self.classify2(distance_w[:self.k]))
+                del distance_w
 
+        if self.weights == "uniform":
+            return self.result
+        elif self.weights == "distance":
+            return self.result_w
 
-    def classify(self,distance):
+    def classify(self, distance):
         label = []
 
         for i in distance:
             label.append(self.y_train[i[0]])
 
-
         return Counter(label).most_common()[0][0]
 
+    def classify2(self, distance):
+        y = np.unique(self.y_train)
+        y_sum = [None]*len(y)
 
+        for j in range(len(y)):
+            sum = 0
+            for i in distance:
+                if y[j] == self.y_train[i[0]]:
+                    sum += i[1]
+            y_sum[j] = sum
+
+        return y[y_sum.index(max(y_sum))]
